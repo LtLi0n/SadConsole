fc76b375b4b3a14e2e88c4afa8e7b7b1a33b5efd
LtLi0n <LtLi0n@users.noreply.github.com>
2019-09-08 02:19:51 +0300

    ContainerBase/ContainerManager provides a consistent way to retrieve or store data.

---
+++ SadConsole/Memory/ContainerBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SadConsole
+{
+    public abstract class ContainerBase
+    {
+        protected readonly IServiceProvider _services;
+
+        public ContainerBase(IServiceProvider services)
+        {
+            _services = services;
+        }
+    }
+
+    public abstract class ContainerBase<T> : ContainerBase where T : class
+    {
+        protected readonly Dictionary<string, T> _items;
+        public IReadOnlyDictionary<string, T> Items => _items;
+
+        public T this[string key]
+        {
+            get => _items.ContainsKey(key) ? _items[key] : null;
+            set
+            {
+                if (_items.ContainsKey(key))
+                {
+                    _items[key] = value;
+                }
+                else
+                {
+                    _items.Add(key, value);
+                }
+            }
+        }
+
+        public ContainerBase(IServiceProvider services) : base(services)
+        {
+            _items = new Dictionary<string, T>();
+        }
+    }
+}
---
+++ SadConsole/Memory/ContainerManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SadConsole
+{
+    public class ContainerManager
+    {
+        private readonly IServiceProvider _services;
+        private readonly IServiceProvider _containers;
+
+        public FontContainer Fonts => GetContainer<FontContainer>();
+
+        public T GetContainer<T>() where T : ContainerBase => _containers.GetService<T>();
+
+        public ContainerManager(IServiceProvider services)
+        {
+            _services = services;
+            _containers = CreateContainers();
+        }
+
+        private IServiceProvider CreateContainers()
+        {
+            ServiceCollection sc = new ServiceCollection();
+            
+            sc.AddSingleton<FontContainer>();
+
+            return sc.BuildServiceProvider();
+        }
+    }
+}
