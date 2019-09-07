6f020b9ee7c3174e11e23fd31f8285c68c3b9405
LtLi0n <LtLi0n@users.noreply.github.com>
2019-09-08 02:21:18 +0300

    Project files.

---
+++ SadConsole/Console.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace SadConsole
+{
+    public class Console
+    {
+        public Console(int width, int height)
+        {
+            ContainerManager containers = new ContainerManager(null);
+        }
+    }
+}
