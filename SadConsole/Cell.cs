8f9039c11310de746bd460d5971766ce4001129d
LtLi0n <LtLi0n@users.noreply.github.com>
2019-09-08 02:21:04 +0300

    Adding Cell. Doesn't contain fancy effects and mirrors just yet.
    Moved Draw method from Cell to SpriteBatchExtensions.

---
+++ SadConsole/Cell.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace SadConsole
+{
+    public struct Cell
+    {
+        public int Glyph { get; set; }
+        public Color Foreground { get; set; }
+        public Color Background { get; set; }
+
+        public bool IsVisible { get; set; }
+
+        public Cell(Color foreground)
+        {
+            Glyph = 0;
+            Foreground = foreground;
+            Background = Color.Black;
+
+            IsVisible = true;
+        }
+
+        public Cell(Color foreground, Color background)
+        {
+            Glyph = 0;
+            Foreground = foreground;
+            Background = background;
+
+            IsVisible = true;
+        }
+
+        public Cell(int glyph, Color foreground, Color background)
+        {
+            Glyph = glyph;
+            Foreground = foreground;
+            Background = background;
+
+            IsVisible = true;
+        }
+
+        ///<summary>Copies the visual appearance to the specified cell. This includes foreground, background, glyph, and mirror effect.</summary>
+        ///<param name="cell">The target cell to copy to.</param>
+        public void CopyAppearanceTo(Cell cell)
+        {
+            cell.Foreground = Foreground;
+            cell.Background = Background;
+            cell.Glyph = Glyph;
+        }
+
+        ///<summary>Sets the foreground, background, glyph, and mirror effect to the same as the specified cell.
+        ///<param name="cell">The target cell to copy from.</param>
+        public void CopyAppearanceFrom(Cell cell)
+        {
+            Foreground = cell.Foreground;
+            Background = cell.Background;
+            Glyph = cell.Glyph;
+        }
+
+        ///<summary>Sets the foreground, background, glyph values to default.</summary>
+        public void SetDefault()
+        {
+            Foreground = Color.White;
+            Background = Color.Black;
+            Glyph = 0;
+        }
+    }
+}
---
+++ SadConsole/Extensions/SpriteBatchExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SadConsole
+{
+    public static class SpriteBatchExtensions
+    {
+        public static void Draw(this SpriteBatch spriteBatch, Cell cell, Point position, Point size, Font font)
+        {
+            if (cell.Background != Color.Transparent)
+            {
+                Rectangle drawingRectangle = new Rectangle(position.X, position.Y, size.X, size.Y);
+
+                spriteBatch.Draw(
+                    font.Image,
+                    drawingRectangle, 
+                    font.GlyphRects[font.SolidGlyphIndex], 
+                    cell.Background, 
+                    0f, 
+                    Vector2.Zero, 
+                    SpriteEffects.None, 
+                    0.3f);
+            }
+        }
+    }
+}
